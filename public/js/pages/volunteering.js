a8ef977ad51eefc5aa98f9c0cb293965ae73f768
Amie Collins <43182941+amiecollins@users.noreply.github.com>
2020-03-23 17:11:17 -0400

    volunteering html complete

--- public/js/pages/volunteering.js
+++ public/js/pages/volunteering.js
@@ -1,17 +1,56 @@
 export default {
     data: function () {
         return {
-            
+            title: "Volunteering Applicant Form",
+            form: [
+                {
+                    name: "Full Name",
+                    type: "name",
+                    value: "",
+                    id: "name"
+                },
+                {
+                    name: "Contact Number",
+                    type: "phone",
+                    value: "",
+                    id: "phone"
+                },
+                {
+                    name: "Email",
+                    type: "email",
+                    value: "",
+                    id: "email"
+                },
+                {
+                    name: "Area of Interest",
+                    type: "text",
+                    value: "",
+                    id: "interest"
+                },
+                {
+                    name: "Message",
+                    type: "textarea",
+                    value: "",
+                    id: "message"
+                }
+            ]
         }
     },
     
     methods: {
-        
+        submitform: function () {
+            // submit form with php
+        }
     },
 
-    template: `<section class="error-page">
-    <h2>Sorry! Something went wrong.</h1>
-    <h3>Our Heroes will do their best to solve it right away</h2>
-    <h4>For now please try another link</h4></section>
-    `
+    template: `
+    <section class="volunteering">
+        <h2 class="volun-title" v-text="title"></h2>
+        <div class="volun-content">
+            <form class="volun-form">
+                <div class="volun-form-item" v-for="item in form"><label for="item.id" v-text="item.name"></label><input type="item.type" value="item.value" id="item.id"></div>
+                <div class="volun-button" @click="submitform">Send</div>
+            </form>
+        </div>
+    </section>`
 }
